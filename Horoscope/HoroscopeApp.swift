665555493dd0fcacd83edc87b3d220058aca46a6
nikitafrolov <nickit.froloff2016@yandex.ru>
2021-02-16 22:24:22 +0300

    Initial Commit

---
+++ Horoscope/HoroscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopeApp.swift
+//  Horoscope
+//
+//  Created by Никита Фролов  on 16.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct HoroscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
